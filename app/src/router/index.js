9eda2194c2f4915f29389c244c5a699804d496d1
Ratchathorn <ratchathorn.s@ku.th>
2022-10-11 12:08:00 +0700

    initial dev branch

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -22,6 +22,14 @@ const router = createRouter({
       path: '/coin-desk',
       name: 'coin-desk',
       component: CoinDeskView
+    },
+    {
+      path: '/test',
+      name: 'test',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Test.vue')
     }
   ]
 })
---
+++ app/src/views/Test.vue
@@ -0,0 +1,31 @@
+<template>
+<div v-for="gold in golds">
+    {{ gold }}
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            title: "gold list",
+            golds: null
+        }
+    },
+    async mounted() {
+        const url = "http://localhost/api/golds"
+
+        try {
+            const response = await axios.get(url)
+            if (response.status == 200) {
+                this.golds = response.data
+            }
+            console.log(response.data)
+        } catch (error) {
+            console.error(error.message)
+        }
+
+    }
+}
+</script>
