054a7b49c3e6459828053b87f42a7afad117c1d6
opaller91 <rinrada.ti@ku.th>
2022-11-01 11:10:23 +0700

    edit withdrawal

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -136,6 +136,21 @@ const router = createRouter({
       name: 'income.edit',
       component: () => import('@/views/income/EditIncomeView.vue')
     },
+    {
+      path: '/withdrawal/create',
+      name: 'withdrawal.create',
+      component: () => import('@/views/withdrawal/CreateWithdrawalView.vue')
+    },
+    {
+      path: '/withdrawal/view',
+      name: 'withdrawal.view',
+      component: () => import('@/views/withdrawal/WithdrawalView.vue')
+    },
+    {
+      path: '/withdrawal/edit/:id',
+      name: 'withdrawal.edit',
+      component: () => import('@/views/withdrawal/EditWithdrawalView.vue')
+    },
   ]
 })
 
--- app/src/views/total/TotalIncomeWithdrawlView.vue
+++ app/src/views/total/TotalIncomeWithdrawlView.vue
@@ -19,14 +19,6 @@
                 <p class="mx-1 mb-3 font-normal text-gray-700 ">วันที่ทำรายการ: {{ selected_income.transaction_date }}</p>
             </div>
         </div>
-        <div v-if="user.role == 'manager'">
-                <a v-bind:href="'/income/edit/'+ selected_income.id" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    แก้ไข
-                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </a>
-        </div>
 
     </div>
     <div v-if="selected_withdrawal != null" class="py-5 grid grid-cols-2">
@@ -45,14 +37,6 @@
                 <p class="mx-1 mb-3 font-normal text-gray-700 ">วันที่ทำรายการ: {{ selected_withdrawal.transaction_date }}</p>
             </div>
         </div>
-        <!-- <div v-if="user.role == 'manager'">
-                <a v-bind:href="'/withdrawal/edit/'+ selected_withdrawal.id" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    แก้ไข
-                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </a>
-        </div> -->
 
     </div>
     <div v-if="selected_withdrawal == null && selected_income == null" class="py-5 grid grid-cols-2">
@@ -246,6 +230,8 @@ export default {
 
         await this.withdrawal_store.fetch()
         this.withdrawals = this.withdrawal_store.getWithdrawals
+
+        await this.employee_store.fetch()
 
         console.log(this.user.role)
         this.role = this.user.role
