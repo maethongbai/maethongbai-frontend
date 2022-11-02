246f9c00719cbfe9c2e55dfc4340ee6c1cd9d71a
Nattapong Phu-utha <Nattapong.phuu@ku.th>
2022-11-02 18:59:11 +0700

    decorate home

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,36 +1,57 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Profile from './components/profile/Profile.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="wrapper max-w-[100%]">
+      
+      <div class="bg-gradient-to-b from-[#D61111] via-[#9B0000] to-[#4E0000] p-5">
+        <div>
+          <div class="relative flex flex-row">
+            <div class="flex-none">
+              <img alt="Maethongbai LOGO" class="logo" src="@/assets/LOGO.png" width="175" height="61.4" />
+            </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <Profile></Profile>
+            <div>
+              <div>
+                <form class="flex items-center min-w-[50%]">   
+                  <label for="simple-search" class="sr-only">Search</label>
+                  <div class="relative w-full">
+                      <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                          <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                      </div>
+                      <input type="text" id="simple-search" class="bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="ค้นหา" required>
+                  </div>
+                  <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-[#FFD12F] rounded-lg hover:bg-[#FFC700] focus:ring-4 focus:outline-none">
+                      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                      <span class="sr-only">Search</span>
+                  </button>
+              </form>
+              </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/test">Test</RouterLink>
-        <RouterLink to="/stock/view">Stock</RouterLink>
-        <RouterLink to="/redemption/view">Redemptions</RouterLink>
-        <RouterLink to="/custom_order/view">Custom Orders</RouterLink>
-        <RouterLink to="/sale/view">Sales</RouterLink>
-        <RouterLink to="/allstock/view">All Stock</RouterLink>
-        <RouterLink to="/withdrawal/view">Withdrawal</RouterLink>
-        <RouterLink to="/income/view">Income</RouterLink>
-        <RouterLink to="/custom_order_worker/view">Custom Order Worker</RouterLink>
-        <RouterLink to="/gold_pattern/view">Gold Pattern</RouterLink>
-        <RouterLink to="/wholesale/view">Wholesale</RouterLink>
-        <RouterLink to="/gold_type/view">Gold Type</RouterLink>
-      </nav>
+              <div class="text-white mt-4">
+                <RouterLink to="/" class="mr-2">หน้าหลัก</RouterLink>
+                <!-- <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/test">Test</RouterLink> -->
+                <RouterLink to="/onsitesale/create" class="m-4">ขายทอง</RouterLink>
+                <RouterLink to="/redemption/create" class="m-4">รับซื้อทอง</RouterLink>
+                <RouterLink to="/income/create" class="m-4">รับเงิน</RouterLink>
+                <RouterLink to="/withdrawal/create" class="m-4">ถอนเงิน</RouterLink>
+                <RouterLink to="/total_sale" class="m-4">สรุปทองที่ขายทั้งหมด</RouterLink>
+                <RouterLink to="/stock/create" class="m-4">เพิ่มทองลงสต็อก</RouterLink>
+                <RouterLink to="/stock/view" class="m-4">สถานะทองที่เพิ่ม</RouterLink>
+              </div>
+                
+            </div>
+
+            <Profile class="text-white pt-2 pl-2 inset-y-0 right-0"></Profile>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
-
   <RouterView />
 </template>
 
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,33 +1,128 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-<div class="block my-5">
-    <router-link to="/redemption/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        Create Redemption
-    </router-link>
-    <router-link to="/custom_order/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        Create Custom Order
-    </router-link>
-    <router-link to="/stock/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        Add Stock
-     </router-link>
-    <router-link to="/onsitesale/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        Create Onsite Sale
-    </router-link>
-    <router-link to="/employee/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        View All Employee
-    </router-link>
-    <router-link to="/total_sale" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        สรุปทองที่ขายทั้งหมด
-        </router-link>
-    <router-link to="/total" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
-        หน้าสรุปรายรับ-รายจ่าย
-    </router-link>
-</div>
-
 <main>
-    <TheWelcome />
+    <div class="flex flex-col mb-10">
+        <div>
+            <h1 class="text-xl m-5">สร้างคำสั่งซื้อ</h1>
+            <div>
+                <router-link to="/onsitesale/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    ขายทอง
+                </router-link>
+                <router-link to="/redemption/create" class="px-5 py-2 my-4 bg-gray-200 rounded-xl">
+                    รับซื้อทอง
+                </router-link>
+                <router-link to="/custom_order/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    สร้างงานสั่งทอง
+                </router-link>
+            </div>
+
+            <h1 class="text-xl m-5">ประวัติคำสั่งซื้อ</h1>
+            <div>
+                <RouterLink to="/redemption/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">รายการซื้อ</RouterLink>
+                <RouterLink to="/sale/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">รายการขาย</RouterLink>
+                <RouterLink to="/custom_order/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">งานสั่ง</RouterLink>
+            </div>
+
+            <h1 class="text-xl m-5">สต็อก</h1>
+            <div>
+                <RouterLink to="/allstock/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    สต็อกทอง
+                </RouterLink>
+                <router-link to="/stock/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    เพิ่มทองลงสต็อก
+                </router-link>
+                <RouterLink to="/stock/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    รายการสินค้าที่ถูกเพิ่มลงสต็อก
+                </RouterLink>
+            </div>
+
+            <h1 class="text-xl m-5">การเงิน</h1>
+            <div>
+                <div>
+                    <RouterLink to="/income/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">เพิ่มรายการรับเงิน</RouterLink>
+                    <RouterLink to="/withdrawal/create" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">เพิ่มรายการเบิกเงิน</RouterLink>
+                </div>
+                <div class="mt-5">
+                    <RouterLink to="/income/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">รายการเงินเข้า</RouterLink>
+                    <RouterLink to="/withdrawal/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">รายการเงินออก</RouterLink>
+                </div>
+            </div>
+
+            <h1 class="text-xl m-5">สรุปการขาย</h1>
+            <div>
+                <router-link to="/total_sale" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    สรุปทองที่ขายทั้งหมด
+                </router-link>
+                <router-link to="/total" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                    หน้าสรุปรายรับ-รายจ่าย
+                </router-link>
+            </div>
+
+
+            <h1 class="text-xl m-5">จัดการข้อมูล</h1>
+            <div>
+                <div>
+                    <RouterLink to="/gold_type/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">ประเภททองทั้งหมด</RouterLink>
+                    <RouterLink to="/gold_pattern/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">ลายทองทั้งหมด</RouterLink>
+                </div>
+
+                <div class="mt-5">
+                    <RouterLink to="/wholesale/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">ร้านขายส่งทั้งหมด</RouterLink>
+                    <RouterLink to="/custom_order_worker/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">ช่างงานสั่งทั้งหมด</RouterLink>
+                </div>
+
+                <div class="mt-5">
+                    <router-link to="/employee/view" class="px-5 py-2 mx-4 my-4 bg-gray-200 rounded-xl">
+                        ดูรายชื่อพนักงานทั้งหมด
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 </template>
+
+<script>
+    import { useUserStore } from '@/stores/user.js'
+    import { useAuthStore } from '@/stores/auth.js'
+    export default {
+      setup() {
+        const auth_store = useAuthStore()
+        const user_store = useUserStore()
+        return { user_store, auth_store }
+      },
+      data() {
+        return {
+            auth: null,
+            users: null,
+            error: null,
+        }
+      },
+      watch: {
+        auth_store: {
+            immediate: true,
+            deep: true,
+            handler(newValue, oldValue) {
+                console.log("in")
+                this.auth = this.auth_store.getAuth
+                this.user = JSON.parse(this.auth_store.getUser)
+            }
+        }
+      },
+      async mounted() {
+        console.log("inn")
+        if (this.auth_store.isAuthen) {
+            this.auth = this.auth_store.getAuth
+            this.user = JSON.parse(this.auth_store.getUser)
+            if (this.user.role == "manager" || this.user.role == "account" || this.user.role == "employee" ) {
+                console.log("authorized " + document.URL);
+            } else {
+                this.$router.push("/");
+            }
+        }
+        else{
+            this.$router.push("/login");
+        }
+      },
+  }
+</script>
+
