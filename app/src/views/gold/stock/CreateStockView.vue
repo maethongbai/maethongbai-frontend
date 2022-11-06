a900ac3a0b29ad2243faef43bf374968cb95929c
Nattapong Phu-utha <Nattapong.phuu@ku.th>
2022-11-06 16:06:28 +0700

    decorate create redemption view

--- app/src/views/gold/stock/CreateStockView.vue
+++ app/src/views/gold/stock/CreateStockView.vue
@@ -6,7 +6,7 @@
         <div class="card-white w-[1200px] text-s1">
             <div class="m-4 ml-0">
                 <!-- breadcrumb -->
-                หน้าหลักระบบหลังร้าน > สต็อกทองทั้งหมด > เพิ่มทองลงสต็อก
+                <b>หน้าหลักระบบหลังร้าน > สต็อกทองทั้งหมด > เพิ่มทองลงสต็อก</b>
             </div>
             <form @submit.prevent="createGold()">
                 <div class="card-gray w-[1044px] mb-4">
